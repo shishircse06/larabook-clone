76359095b564407c1481e3c881ee7b62df66a30d
Shishir Mazumder <shishir_ruet@yahoo.com>
2015-01-14 17:12:43 +0600

    setting up home page

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+    {{ HTML::style('css/main.css') }}
     <title>Document</title>
 </head>
 <body>
+@include('layouts.partials.nav')
 <div class="container">
     @yield('content')
 </div>
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.default')
 @section('content')
-    Welcome to larabook.
+    <div class="jumbotron">
+        <h1>Larabook demo</h1>
+        <p class="lead">Learning laravel from jeffrey way series larabook .</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up</a></p>
+    </div>
 @stop
