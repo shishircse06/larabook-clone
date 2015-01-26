fa491e664a07436020d9830332861502dc094e5a
Shishir Mazumder <shishir_ruet@yahoo.com>
2015-01-26 20:10:28 +0600

    hashing password

--- app/Larabook/Users/User.php
+++ app/Larabook/Users/User.php
@@ -4,8 +4,12 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
-use Eloquent;
+use Eloquent,Hash;
 
+/**
+ * Class User
+ * @package Larabook\Users
+ */
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
@@ -24,6 +28,12 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var array
 	 */
 	protected $hidden = array('password', 'remember_token');
+
+	//password hashing function
+	public function setPasswordAttribute($password){
+		$this->attributes['password']=Hash::make($password);
+
+	}
 
 	public static function register($username,$email,$password){
 		$user=new static(compact('username','email','password'));
